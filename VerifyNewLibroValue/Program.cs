a06fd8da7490ebca2bc5c8b611081c283b590c6a
Omar-Sebastian28 <omarjoaquinminayasebastian@gmail.com>
2025-09-28 19:13:31 -0400

    Configuracion del program para el  Azure Functions

--- VerifyNewLibroValue/Program.cs
+++ VerifyNewLibroValue/Program.cs
@@ -1,7 +1,11 @@
+using Bliblioteca.Infraestructura.Persistencia;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Bliblioteca.Core.Aplication;
+using Biblioteca.Infraestructura.Identity;
+using BIblioteca.Infraestructura.Share;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
@@ -11,4 +15,14 @@ builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
 
-builder.Build().Run();
+//Extension method.
+builder.Services.AddPersistenceInfrastructure(builder.Configuration);
+builder.Services.AddAplicationServices();
+builder.Services.AddHealthChecks();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddShareLayerIoc(builder.Configuration);
+builder.Services.AddLayerIdentityForWebApp(builder.Configuration);
+
+
+await builder.Build().RunAsync();
