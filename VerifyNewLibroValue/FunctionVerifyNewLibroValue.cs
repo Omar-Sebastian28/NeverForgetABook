88b18f271b3e96fdb928f6c7c7aefdb4e6215115
Omar-Sebastian28 <omarjoaquinminayasebastian@gmail.com>
2025-10-01 18:15:25 -0400

    Reparacion de los json en el local.settings.json

--- VerifyNewLibroValue/FunctionVerifyNewLibroValue.cs
+++ VerifyNewLibroValue/FunctionVerifyNewLibroValue.cs
@@ -1,4 +1,6 @@
+using Bliblioteca.Core.Aplication.Interfaces;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace VerifyNewLibroValue;
@@ -6,20 +8,41 @@ namespace VerifyNewLibroValue;
 public class FunctionVerifyNewLibroValue
 {
     private readonly ILogger _logger;
+    private readonly ILibroServices _libroServices;
 
-    public FunctionVerifyNewLibroValue(ILoggerFactory loggerFactory)
+
+    public FunctionVerifyNewLibroValue(ILoggerFactory loggerFactory, ILibroServices libroServices)
     {
         _logger = loggerFactory.CreateLogger<FunctionVerifyNewLibroValue>();
+        _libroServices = libroServices; 
     }
 
+
     [Function("FunctionVerifyNewLibroValue")]
-    public void Run ([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer)
-    {  
-        _logger.LogInformation("C# Timer trigger function executed at: {ExecutionTime}", DateTime.Now);
-                                                                                                                                                                                                                                                                                                                                                                      
+    public async Task Run ([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer)
+    {
         if (myTimer.ScheduleStatus is not null)
         {
             _logger.LogInformation("Next timer schedule at: {NextSchedule}", myTimer.ScheduleStatus.Next);
+        }
+
+        _logger.LogInformation("C# Timer trigger function executed at: {ExecutionTime}", DateTime.Now);
+
+        if (myTimer.IsPastDue)
+        {
+            _logger.LogWarning("The timer is past due!");
+        }
+
+        else 
+        {
+            var libros = _libroServices.GetQuery();
+            if (!await libros.AnyAsync()) 
+            {
+              _logger.LogWarning("No hay libros registrados.");
+            }
+
+            var libroAnioActual = libros.Where(l => l.AñoPublicacion == DateTime.UtcNow.Year).ToList();
+            _logger.LogInformation("Timer is on time.");
         }
     }
 }
